8a6467df39e730612177eb45168d5ce06c742b71
dltldnr2563 <dltldnr2563@naver.com>
2025-08-06 11:44:10 +0900

    "Entity, Repository 추가"

---
+++ springboot/src/main/java/com/example/springboot/Product/model/ProductEntity.java
@@ -0,0 +1,52 @@
+package com.example.springboot.Product.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idx;
+
+    private String name;
+
+    private Integer price;
+
+    private String description;
+
+
+    public Integer getIdx() {
+        return idx;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setIdx(Integer idx) {
+        this.idx = idx;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
