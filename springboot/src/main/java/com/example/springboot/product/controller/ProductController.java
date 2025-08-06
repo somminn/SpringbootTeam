cb5451601e47d71c8c77919777fbbe3cd40af7ca
wnstjs1031 <wnstjs1031@naver.com>
2025-08-06 11:45:56 +0900

    Controller 기본 기능

--- springboot/src/main/java/com/example/springboot/product/controller/ProductController.java
+++ springboot/src/main/java/com/example/springboot/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.springboot.product.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,17 @@ public class ProductController {
     public ResponseEntity save(@RequestMapping BoardDto.Create dto) {
         boardService.save(dto);
         return ResponseEntity.status(200).body("완료");
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity read(Integer idx) {
+        BoardDto.Read result = boardService.read(idx);
+        return ResponseEntity.status(200).body(result);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity list() {
+        List<BoardDto.Read> result = boardService.list();
+        return  ResponseEntity.status(200).body(result);
     }
 }
