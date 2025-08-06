e983e106c3007b1db3d152463add0d8dd18709e3
somminn <cowmin104@gmail.com>
2025-08-06 13:50:47 +0900

    test

--- springboot/src/main/java/com/example/springboot/product/model/ProductEntity.java
+++ springboot/src/main/java/com/example/springboot/product/model/ProductEntity.java
@@ -1,29 +1,16 @@
 package com.example.springboot.product.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idx;
-
     private String name;
-
-    private Integer price;
 
     private String contents;
 
-    public Integer getIdx() {
-        return idx;
-    }
-
-    public void setIdx(Integer idx) {
-        this.idx = idx;
-    }
+    private Integer price;
 
     public String getName() {
         return name;
@@ -33,19 +20,19 @@ public class ProductEntity {
         this.name = name;
     }
 
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
     public String getContents() {
         return contents;
     }
 
     public void setContents(String contents) {
         this.contents = contents;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
     }
 }
